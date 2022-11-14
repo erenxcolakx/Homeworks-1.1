e3671ceb83fac92b2a8d15aeb963f7811e54c862
Eren Çolak <erenncolak@hotmail.com>
2022-11-14 20:54:43 +0300

    Homework 1 Problem 1 added

---
+++ Pro1_150121015.java
@@ -0,0 +1,16 @@
+
+public class Pro1_150121015 {
+
+	public static void main(String[] args) {
+			System.out.println(" \\\\\\\\//////////"); // Hair
+			System.out.println(" =============");  // Head band
+			System.out.println(" | ~~~   ~~~ |     ----- ");  // Eyebrows
+			System.out.println(" | <O>   <O> |   / Hello \\ ");  // Eyes
+			System.out.println("(|     ^     |) <  Junior |" );//Nose, ears
+			System.out.println(" |   //-\\\\   |   \\ Coder!/"); //Muscle, mouth
+			System.out.println("  \\---------/      -----"); 
+			System.out.println("   \\  JR.  /"); 
+					}
+		/* Eren Çolak
+		   150121015  */ 
+	}
